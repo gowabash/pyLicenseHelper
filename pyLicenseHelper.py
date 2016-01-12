943007a8c640621edaad06fba0336cee8313bbba
Matt Brown <gowabash@gmail.com>
2016-01-12 14:33:19 -0500

    now writes to a files as csv.  refactored arg parsing also

--- pyLicenseHelper.py
+++ pyLicenseHelper.py
@@ -2,33 +2,55 @@
 import pkg_resources
 import sys
 import getopt
+import argparse
+import csv
+
+HEADERS = ["Version", "Homepage", "License", "package"]
+
+
+def get_parser():
+    description = 'Get the licenses for your requirements.'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--package',
+                        dest='package',
+                        action='store',
+                        help='name of the package to be scanned')
+    parser.add_argument('--csv',
+                        dest='csv_file',
+                        action='store',
+                        help='name of the file to write to')
+    parser.add_argument('--req',
+                        dest='requirements_file',
+                        action='store',
+                        help='name of the requirements.txt file')
+    return parser
 
 
 def main(initial_args):
-    requirements_file = ""
-    package = ""
-    try:
-        opts, args = getopt.getopt(initial_args,
-                                   "hr:p:",
-                                   [])
-    except getopt.GetoptError:
-        print 'test.py -r <requirements file> -p <individual package>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'test.py -r <requirements file> -p <individual package>'
-            sys.exit()
-        elif opt in ("-r"):
-            requirements_file = arg
-            if len(requirements_file) > 0:
-                with open(requirements_file) as f:
-                    content = f.readlines()
-                    for line in content:
-                        stuff = line.split('==', 1)
-                        get_pkg_license(stuff[0])
-        elif opt in ("-p"):
-            package = arg
-            get_pkg_license(package)
+    parser = get_parser()
+    cooked_args = parser.parse_args(initial_args)
+
+    if cooked_args.package:
+        get_pkg_license(cooked_args.package)
+    elif cooked_args.requirements_file:
+        package_infos = [HEADERS]
+        with open(cooked_args.requirements_file) as f:
+            content = f.readlines()
+            for line in content:
+                package = line.split('==', 1)[0]
+                package_infos.append(get_pkg_license(package))
+        if cooked_args.csv_file:
+            write_csv(package_infos, cooked_args.csv_file)
+        else:
+            print package_infos
+    else:
+        parser.print_help()
+
+
+def write_csv(data, file):
+    with open(file, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 
 
 def get_pkg_license(package):
@@ -39,7 +61,7 @@ def get_pkg_license(package):
     try:
         pkgs = pkg_resources.require(package)
     except pkg_resources.DistributionNotFound:
-        print info.values()
+        return info.values()
         return
     pkg = pkgs[0]
     meta_files_to_check = ['PKG-INFO', 'METADATA']
@@ -56,7 +78,7 @@ def get_pkg_license(package):
                     pass
     except:
         pass
-    print info.values()
+    return info.values()
 
 
 if __name__ == "__main__":
